9cb08ac219bcfbdd5550895536f2703ad4906545
SimsEmile <90972429+SimsEmile@users.noreply.github.com>
2023-06-02 10:40:40 +0200

    Update test.py
    color

---
+++ test.py
@@ -0,0 +1,64 @@
+import RPi.GPIO as GPIO
+import time
+
+
+s2 = 23
+s3 = 24
+signal = 25
+NUM_CYCLES = 10
+
+
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(s2,GPIO.OUT)
+  GPIO.setup(s3,GPIO.OUT)
+  print("\n")
+  
+
+
+
+
+def loop1():
+  temp = 1
+  while(1):  
+
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.LOW)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start 
+    red  = NUM_CYCLES / duration   
+   
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    blue = NUM_CYCLES / duration
+    
+
+    GPIO.output(s2,GPIO.HIGH)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    green = NUM_CYCLES / duration
+    
+      
+    if green>12000 and blue>12000 and red>12000:
+      print("white")
+    if green <7000 and blue < 7000 and red < 7000:
+      print("black")
+      
+for loop in range(125):
+    loop1()
+
+if KeyboardInterrupt:
+  GPIO.cleanup()
