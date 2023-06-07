b0deaf25ac23fa1cf579f8e33c24ec69ad842e96
SimsEmile <90972429+SimsEmile@users.noreply.github.com>
2023-06-07 16:43:39 +0200

    Update primary.py

--- primary.py
+++ primary.py
@@ -7,6 +7,10 @@ Created on Wed May 24 14:32:32 2023
 
 import RPi.GPIO as GPIO
 import time
+import board
+import busio
+import adafruit_vl6180x
+
 
 #set GPIO
 GPIO.setmode(GPIO.BCM)
@@ -19,9 +23,7 @@ Motor2 = {'EN': 11, 'input1': 9, 'input2': 10} #motor 2 is used for the belt, wi
 S2 = 5
 S3 = 6
 OUT = 18
-#set GPIO pins for the ultrasound sensor
-Trig_pin = 2
-Echo_pin = 3
+
 #set GPIO pins as output
 GPIO.setup(S2, GPIO.OUT)
 GPIO.setup(S3, GPIO.OUT)
@@ -48,35 +50,27 @@ exitcode = None
 binary_index = len(binary)-1
 
 def ultrasoundcheck():
-  GPIO.setup(Trig_pin, GPIO.HIGH)
-  time.sleep(0.0001)
-  GPIO.setup(Trig_pin, GPIO.LOW)
-  
-  time_start = time.time()
-  time_end = time.time()
-  while GPIO.input(Echo_pin) == 0:
-      time_start = time.time()
-  while GPIO.input(Echo_pin) == 1:
-      time_end = time.time()
-      
-  duration = time_end - time_start
-  distance = duration * 17150
-  start = time.time()    
-  if 8.75 < GPIO.input(OUT) < 10.75: #Checks if the distance (including slight reading errors, is sufficient to consider there being a disk)
-    time.sleep(3.5) #creates a time interval, after which normally, the disk would've already passes, and thus nothing would be retrieved
-    if  8.75 < GPIO.input(OUT) < 10.75: #if there still is something after the time interval, we know that sth might be off
-      print("Shape does not seem to be a disk, wait 10 seconds") #send message to terminal (which will be on the computer)
-      time.sleep(10) #be inactive for 10 seconds, then proceed further with the rest.
-    else:
-      colorCheck()
-  else:
-    pass
+    range_mm = sensor.range
+    print("Range: {0}mm".format(range_mm))
+    if range_mm == 0:
+      first_time = time.time()
+      while range_mm == 0:
+        range_mm = sensor.range
+        if time.time() - first_time > 3:
+          range_mm = 1
+          time.sleep(20)
+          if sensor.range == 0:
+            print("belt is potentially stuck, or factory has tried to sabotage me too much, initiating shutdown")
+      time.sleep(2.5)
+      color = ColorReading()
+      Colorcheck()
+    
 
 def colorCheck():
   color = Colorreading()
   if (color == 'black' and binary[binary_index] == 0) or (color == "white" and binary[binary_index] == 1): #check if disc is the correct color we need
     start = time.time() #reset timer, as if time.time() - start = 30, we know there is a problem.
-    time.sleep(0.5) #create a time buffer, as to not force the motor to be on for too long
+    time.sleep(5) #create a time buffer, as to not force the motor to be on for too long
     Arm() #call upon the arm function, which activates the windmill
   else:
     print("Color readings are different from described color, let it pass through") #message to the terminal, informs user that the disk was a wrong one
@@ -86,28 +80,28 @@ def Arm():
     GPIO.setup(Motor1[x], GPIO.OUT) #sets all GPIO pins to output pins (information is sent out, not retrieved)
     GPIO.setup(Motor2[x], GPIO.OUT) #same, except for the motor of the belt
 
-    EN1 = GPIO.PWM(Motor1['EN'], 100)
-    EN2 = GPIO.PWM(Motor2['EN'], 100)
+  EN1 = GPIO.PWM(Motor1['EN'], 100)
+  EN2 = GPIO.PWM(Motor2['EN'], 100)
 
-    EN1.start(0)
-    EN2.start(0)
+  EN1.start(0)
+  EN2.start(0)
+  time.sleep(15)
+  print ("FORWARD MOTION")
+  EN1.ChangeDutyCycle(30)
+  EN2.ChangeDutyCycle(80)
 
-    print ("FORWARD MOTION")
-    EN1.ChangeDutyCycle(30)
-    EN2.ChangeDutyCycle(80)
+  GPIO.output(Motor1['input1'], GPIO.HIGH)
+  GPIO.output(Motor1['input2'], GPIO.LOW)
 
-    GPIO.output(Motor1['input1'], GPIO.HIGH)
-    GPIO.output(Motor1['input2'], GPIO.LOW)
-
-    GPIO.output(Motor2['input1'], GPIO.HIGH)
-    GPIO.output(Motor2['input2'], GPIO.LOW)
-    time.sleep(5)
+  GPIO.output(Motor2['input1'], GPIO.HIGH)
+  GPIO.output(Motor2['input2'], GPIO.LOW)
+  time.sleep(5)
     
-    print("Stop motors")
-    EN1.ChangeDutyCycle(0)
-    EN2.ChangeDutyCycle(0)
+  print("Stop motors")
+  EN1.ChangeDutyCycle(0)
+  EN2.ChangeDutyCycle(0)
     
-    NextNumber() #After moving the arm, we continue onto the next number
+  NextNumber() #After moving the arm, we continue onto the next number
   
 
 def NextNumber():
